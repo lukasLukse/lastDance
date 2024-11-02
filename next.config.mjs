28a5bf526384c48eb80570c8eec2f98dfc6df23c
unknown <lukas.lasas1@gmail.com>
2024-11-03 00:54:38 +0200

    Added server

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    SERVER_URL: "https://galutinisdarbas.onrender.com",
+    SERVER_URL: "https://galutinisdarbas-1.onrender.com",
     JWT_KEY: "questions_app_jwt",
   },
 };
